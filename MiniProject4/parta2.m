3c09a2dad72cb1b90931ad2ac74460d1b60466d2
martin <martin.alejo@aol.com>
2022-12-05 01:02:09 -0800

    part B

--- MiniProject4/parta2.m
+++ MiniProject4/parta2.m
@@ -1,8 +1,6 @@
 r = 10000;
 c = 1.59154943092e-9;
 am = 1.58578643763;
-s = tf('s');
+s = 1i;
 sys=am*((1/(r*c)^2)/(s^2+s*((3-am)/(r*c))+1/(r*c)^2));
-
-rlocusplot(sys)
-axis([-2e5 2e5 -2e5 2e5])
+plot(real(sys),imag(sys))
