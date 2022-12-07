328a64606a2f9d35da703e1903b7830a82319814
martin <martin.alejo@aol.com>
2022-12-07 13:27:24 -0800

    added part 3.2

--- MiniProject4/partA.m
+++ MiniProject4/partA.m
@@ -3,6 +3,7 @@ c = 1.59154943092e-9;
 am = 1.58578643763;
 s = tf('s');
 sys=am*((1/(r*c)^2)/(s^2+s*((3-am)/(r*c))+1/(r*c)^2));
-
+hold on
 rlocusplot(sys)
-axis([-2e5 2e5 -2e5 2e5])
+set(findall(gcf,'Type','line'),'LineWidth',3)
+
